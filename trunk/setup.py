a7dfb2297e14f4d49063257bcefb2c10049fa99c
vfoley <vfoley@00b3cb69-ec50-0410-ada5-090181990bec>
2008-07-03 23:44:17 +0000

    - More reordering :)

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,7 +1,8 @@
 from distutils.core import setup, Extension
 
 module1 = Extension('_unpack',
-                    sources=['pyunpack.c', 'unpack.c'])
+                    sources=['src/pyunpack.c',
+                             'src/unpack.c'])
 
 setup(name='Broodwar Replay File Unpacker',
       version='0.1',
