df41ff51bf83bc79a2cee768f05664cfd4eaeaee
vfoley <vfoley@00b3cb69-ec50-0410-ada5-090181990bec>
2008-07-04 00:53:04 +0000

    - Added test runner

--- trunk/setup.py
+++ trunk/setup.py
@@ -4,7 +4,8 @@ module1 = Extension('_unpack',
                     sources=['src/pyunpack.c',
                              'src/unpack.c'])
 
-setup(name='Broodwar Replay File Unpacker',
-      version='0.1',
-      description='Helper C module.',
-      ext_modules=[module1])
+if __name__ == '__main__':
+    setup(name='Broodwar Replay File Unpacker',
+          version='0.1',
+          description='Helper C module.',
+          ext_modules=[module1])
--- trunk/tests/test_datatypes.py
+++ trunk/tests/test_datatypes.py
@@ -1,31 +1,31 @@
 import unittest
 
-import datatypes
+from pyreplib import datatypes
 
 class TestDataTypes(unittest.TestCase):
     def test_byte(self):
         byte = datatypes.Byte(1)
         self.assertEquals(byte.n, 1)
-        self.assertEquals(byte.unpack_string, '1B')
+        self.assertEquals(byte.get_unpack_string(), '1B')
         byte.n = 12
         self.assertEquals(byte.n, 12)
-        self.assertEquals(byte.unpack_string, '12B')
+        self.assertEquals(byte.get_unpack_string(), '12B')
 
     def test_word(self):
         word = datatypes.Word(1)
         self.assertEquals(word.n, 1)
-        self.assertEquals(word.unpack_string, '1H')
+        self.assertEquals(word.get_unpack_string(), '1H')
         word.n = 12
         self.assertEquals(word.n, 12)
-        self.assertEquals(word.unpack_string, '12H')
+        self.assertEquals(word.get_unpack_string(), '12H')
 
     def test_dword(self):
         dword = datatypes.DWord(1)
         self.assertEquals(dword.n, 1)
-        self.assertEquals(dword.unpack_string, '1I')
+        self.assertEquals(dword.get_unpack_string(), '1I')
         dword.n = 12
         self.assertEquals(dword.n, 12)
-        self.assertEquals(dword.unpack_string, '12I')
+        self.assertEquals(dword.get_unpack_string(), '12I')
 
 if __name__ == '__main__':
     unittest.main()
