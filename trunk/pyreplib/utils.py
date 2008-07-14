9202b7e4f2bce0aa18f745319f1b23c07fe3dc6d
vfoley <vfoley@00b3cb69-ec50-0410-ada5-090181990bec>
2008-07-14 00:52:03 +0000

    - Added apm_stats() and actions_per_minute()

--- trunk/pyreplib/utils.py
+++ trunk/pyreplib/utils.py
@@ -1,1 +1,30 @@
+def avg(L):
+    if not L:
+        return 0
+    else:
+        return sum(L) / len(L)
 
+def actions_per_minute(player):
+    '''
+    Yield the number of actions for each minute of the game for `player`.
+    '''
+    one_minute = 24 * 60
+    minute = one_minute
+    n = 0
+    for action in player.actions:
+        if action.tick < minute:
+            n += 1
+        else:
+            yield n
+            n = 0
+            minute += one_minute
+
+def apm_stats(player):
+    '''
+    Return the minimum, average and maximum number of actions
+    per minutes for `player`.
+    '''
+    actions = list(actions_per_minute(player))
+    return (min(actions),
+            avg(actions),
+            max(actions))
